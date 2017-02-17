1c9b30cf487521a606941e6ffac8288b5567cb51
Ollie Etherington <ollie.etherington@gmail.com>
2017-02-17 19:54:59 +0000

    Added image carousel to main docs page and improved UI

---
+++ docs/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".chrd_carousel").slick({
+		dots: true,
+		infinite: true,
+		fade: true,
+		lazyLoad: "ondemand",
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3500,
+	});
+});
